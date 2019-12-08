97f5d85c640cfd8aca7af99e59a716218ae13c7a
diliproshitha <diliproshitha@gmail.com>
2019-12-08 18:51:58 +0530

    Initial Commit

---
+++ src/main/java/tech/devdiaries/msscbeerservice/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.devdiaries.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceApplication.class, args);
+    }
+
+}
